a0c2b5ae70f1b22d09586732bab97790416c7cfb
PrakarshaKondour <kondourprakarsha@gmail.com>
2025-11-12 13:37:27 +0000

    made a lil comment

--- app.py
+++ app.py
@@ -16,3 +16,4 @@ if __name__=="__main__":
 
 # use of jinja is that use python here     for that put {{}}
 # first thing we do is code space then upload the files cssinto static and html to templates and then whererver there is href for css add static/app.css kinda then for html we need to replace with route that is including py {{url_for('projects')}}
+#then click on source control button (branch logo) click on commit ,click yes,write a msg then click smalll tick
