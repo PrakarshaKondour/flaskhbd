059b95f71361e904ad96926de2ed18ada5fd6a4c
PrakarshaKondour <kondourprakarsha@gmail.com>
2025-11-12 13:35:05 +0000

    added files (initial thing)

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("project(hbd).html")
+
+@app.route("/form")
+def home():
+    return render_template("form.html")
+
+@app.route("/gallery")
+def home():
+    return render_template("gallery.html")
+if __name__=="__main__":
+    app.run(debug==True)
+
+# use of jinja is that use python here     for that put {{}}
+# first thing we do is code space then upload the files cssinto static and html to templates and then whererver there is href for css add static/app.css kinda then for html we need to replace with route that is including py {{url_for('projects')}}
